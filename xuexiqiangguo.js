96f17b8bd036ed21d346cf3f716e061412d6b2c0
无星 <32910694@qq.com>
2020-10-18 19:39:36 +0800

    添加视频

--- xuexiqiangguo.js
+++ xuexiqiangguo.js
@@ -2,42 +2,16 @@ function lauchXueXi(){
     // 打开学习强国
     launchApp("学习强国")
 }
-// 跳转到学习页面
-function goToHome(){
-    id("home_button_tab_button_work").findOne().click();
-}
-// 跳转到电视台
-function goToTV(){
-    id("home_button_tab_button_contact").findOne().click();
-}
-// 获取之前几天的日期,num=0代表今天,1是昨天,2是前天
-function getTime(num) {
-    let day = new Date();
-    day.setTime(day.getTime()-num*24*60*60*1000);
-    let s1 = day.getFullYear()+"-" + (day.getMonth()+1) + "-" + day.getDate();
-    return s1
-    // console.log("当前时间是" + curTime.getFullYear() + "-" + (curTime.getMonth() + 1) + "-" + curTime.getDate() + "(周" + curTime.getDay() + ") " + curTime.getHours() + ":" + curTime.getMinutes() + ":" + curTime.getSeconds());
-}
-// 返回上一页面
-function goBack() {
-    back()
-}
-// 任务1,选读文章
-// 6篇文章,每篇1分钟
-function XuanDuWenZhang(){
+// 列表滚动(需要寻找的文字,不可能出现的左位移,滚动控件)
+function touchList(findText,unleft,scrollIndex){
     let maxRead = 6;
     let readSum = 0;
-    // 1.切换到要闻
-    className("android.widget.TextView").text("要闻").findOne().parent().click();
-    sleep(2000);
-    // 获取viewPager
     let viewBounds = id("view_pager").findOne(2000).bounds();
     console.log("找viewBounds");
-    // 2.捕获播报按钮
     while(readSum < maxRead){
         let tmpArray = boundsInside(viewBounds.left,viewBounds.top,viewBounds.width(),viewBounds.height())
         .className("android.widget.TextView")
-        .text("播报")
+        .text(findText)
         .find()
         console.log("tmpArray==="+tmpArray.length);
         // 去重,判断高度即可,每个高度肯定都不一样
@@ -45,7 +19,7 @@ function XuanDuWenZhang(){
         let workButtonArray = [];
         tmpArray.forEach(child => {
             let bounds = child.bounds()
-            if(bounds.left !== 0 && tmpBoundsArray.indexOf(bounds.top) === -1){
+            if(bounds.left !== unleft && tmpBoundsArray.indexOf(bounds.top) === -1){
                 // 不存在
                 workButtonArray.push(child);
                 tmpBoundsArray.push(bounds.top)
@@ -65,20 +39,60 @@ function XuanDuWenZhang(){
                 console.log("不可点击");
             }
         })
-        scrollDown(3);
+        console.log("readSum:"+readSum+"maxRead:"+maxRead);
+        scrollDown(scrollIndex);
         sleep(2000);
     }
+}
+// 跳转到学习页面
+function goToHome(){
+    id("home_bottom_tab_button_work").findOne().click();
+}
+// 跳转到电视台
+function goToTV(){
+    console.log("跳转电视台");
+    id("home_bottom_tab_button_contact").findOne().click()
+}
+// 获取之前几天的日期,num=0代表今天,1是昨天,2是前天
+function getTime(num) {
+    let day = new Date();
+    day.setTime(day.getTime()-num*24*60*60*1000);
+    let s1 = day.getFullYear()+"-" + (day.getMonth()+1) + "-" + day.getDate();
+    return s1
+    // console.log("当前时间是" + curTime.getFullYear() + "-" + (curTime.getMonth() + 1) + "-" + curTime.getDate() + "(周" + curTime.getDay() + ") " + curTime.getHours() + ":" + curTime.getMinutes() + ":" + curTime.getSeconds());
+}
+// 返回上一页面
+function goBack() {
+    back()
+}
+// 任务1,选读文章
+// 6篇文章,每篇1分钟
+function XuanDuWenZhang(){
+    // 1.切换到要闻
+    goToHome();
+    sleep(2000);
+    className("android.widget.TextView").text("要闻").findOne().parent().click();
+    sleep(2000);
+    touchList("播报",0,3)
 }
 
 function ShiTingXueXi(){
     goToTV();
     sleep(2000);
-    let maxRead = 6;
-    let readSum = 0;
     // 1.切换到要闻
     className("android.widget.TextView").text("联播频道").findOne().parent().click();
+    sleep(2000);
+    touchList("中央广播电视总台",1116,7)
 }
-
+// 找寻滚动数据
+function testScroll(){
+    for (let index = 0; index < 100; index++) {
+        sleep(1000);
+        log(index);
+        scrollDown(index);
+        sleep(1000);
+    }
+}
 // 阅读文章,第一个是日期,第二个是总数
 function readPage(readSum,viewBounds){
     let workButtonArray = className("android.widget.TextView")
@@ -110,9 +124,9 @@ function main(){
     // lauchXueXi();
     // sleep(5000);
     // 任务1:选读文章6篇
-    // XuanDuWenZhang();
+    XuanDuWenZhang();
     // 任务2，视听学习
-    ShiTingXueXi();
+    // ShiTingXueXi();
 }
 
 main();
