8cc2e3858cb85013d1d32019aa89a8e456b544e3
无星 <wuxing@bjca.org.cn>
2020-11-06 14:53:38 +0800

    添加飞书

---
+++ feishu-shangban.js
@@ -0,0 +1,33 @@
+
+// 唤醒机器
+function keepDrow(){
+    device.wakeUpIfNeeded();
+    device.keepScreenOn();
+    if(!device.isScreenOn()){
+        device.wakeUpIfNeeded();
+        keepDrow();
+    }
+}
+
+// 上划解锁
+function threeFingerSwipeUp(){
+    swipe(500,1890,500,1100,501)
+}
+
+// 唤醒飞书
+function lauchFeishu(){
+    launchApp("飞书")
+}
+
+// 下班打卡
+function main(){
+    // 唤醒屏幕
+    keepDrow();
+    sleep(2000);
+    // 解锁
+    threeFingerSwipeUp();
+    sleep(2000);
+    // 加载飞书
+    lauchFeishu();
+}
+main();
